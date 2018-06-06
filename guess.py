2f8127d0e526330c567c0403f47a410395f1c098
Keith <keith@unixloft.com>
2018-06-06 11:18:22 -0500

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,35 @@
+
+########################################
+### GAME LOGIC / FLOW                ###
+########################################
+# 1. Ask user for minimum number
+# 2. Ask user for maximum number
+# 3. Pick random number between minimum, maximum
+# 4. Ask user to guess number
+# 5. If guessed number is equal to random number, player wins!
+# 6. If guessed number is NOT equal to random number, player loses!
+# 7. Display win or lose to player
+# 8. Ask to play again, if so then repeat from step 3.
+
+########################################
+### HELPER CODE                      ###
+########################################
+# 1. PRINT TO CONSOLE
+# ------------------------
+# print("\t**********************************************")
+# print("\t***  Hello Player                          ***")
+# print("\t**********************************************")
+
+# 2. GET RANDOM NUMBER BETWEEN MIN/MAX (e.g. 1 and 10)
+# ------------------------
+# import random
+# print(random.randint(1, 10))
+
+# 3. LOOP UNTIL YOU PRESS Q
+# ------------------------
+# while True:
+#     print("\t Type Q to Quit")
+#     n = input("Enter text: ")
+#     if n == 'Q':
+#         break
+
